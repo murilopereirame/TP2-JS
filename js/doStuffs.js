b37ad1db520548481de9e67e1b91099f4b2d434a
ga-sr <gabriel.10rocha@hotmail.com>
2019-12-03 11:01:13 -0300

    added click for review

--- js/doStuffs.js
+++ js/doStuffs.js
@@ -1,42 +1,46 @@
 function moveme(element) {
-    
-    let currentPos = element.parentNode.id;    
-    
-    if(currentPos == "toDo") {
+
+    let currentPos = element.parentNode.id;
+
+    if (currentPos == "toDo") {
         element.remove();
         document.getElementById("doing").appendChild(element);
-    } else if(currentPos == "doing") {
+    } else if (currentPos == "doing") {
+        element.remove();
+        document.getElementById("review").appendChild(element);
+    }
+    else if (currentPos == "review") {
         element.remove();
         document.getElementById("complete").appendChild(element);
-    }    
-    
+    }
+
 }
 
 function newTask() {
     document.getElementById("newTaskContainer").style.display = "block";;
-    document.getElementById("content").style.filter = "blur(8px)";
+    document.getElementById("content").style.filter = "blur(2px)";
 }
 
-document.addEventListener ('keydown', (event) => {
+document.addEventListener('keydown', (event) => {
     const keyName = event.keyCode;
     console.log(keyName);
-    if(keyName == 13 && event.ctrlKey) {
-        if(document.getElementById("newTaskContainer").style.display == "block") {
+    if (keyName == 13 && event.ctrlKey) {
+        if (document.getElementById("newTaskContainer").style.display == "block") {
             let node = document.createElement('div');
             let taskName = document.getElementById("taskName").value;
             let taskDesc = document.getElementById("taskDesc").value;
             node.className = "task"
-            node.innerHTML = 
-            "<span class=\"taskName\">"+taskName+"</span>" +
-            "<br>" +
-            "<span class=\"taskDesc\">"+taskDesc+"</span>";
-            node.onclick = function() { moveme(node); };
+            node.innerHTML =
+                "<span class=\"taskName\">" + taskName + "</span>" +
+                "<br>" +
+                "<span class=\"taskDesc\">" + taskDesc + "</span>";
+            node.onclick = function () { moveme(node); };
             document.getElementById("toDo").appendChild(node);
             document.getElementById("taskName").value = "";
             document.getElementById("taskDesc").value = "";
         }
-    } else if(keyName == 27) {
-        if(document.getElementById("newTaskContainer").style.display == "block") {
+    } else if (keyName == 27) {
+        if (document.getElementById("newTaskContainer").style.display == "block") {
             document.getElementById("newTaskContainer").style.display = "none";
             document.getElementById("content").style.filter = "";
             document.getElementById("taskName").value = "";
